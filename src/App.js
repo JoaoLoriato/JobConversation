73cbda08811f53eaa1ac91cbc92221b1c53d17de
Your Name <joaoloriato@outlook.com>
2018-12-19 16:29:41 -0200

    Installing Provider and Redux

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import React from 'react';
+import {Provider} from 'react-redux';
+import {createStore} from 'redux';
+
 import Routes from './Routes';
 
 export default props => (
+    <Provider store={createStore(reducers)}>
     <Routes />
+    </Provider>    
 );
