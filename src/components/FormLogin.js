64421aac05bb9dfabf06f28881a08f5d899ee2b9
Your Name <joaoloriato@outlook.com>
2018-12-20 14:37:00 -0200

    Changing the component states through the ActionsCreators

--- src/components/FormLogin.js
+++ src/components/FormLogin.js
@@ -2,17 +2,17 @@ import React from 'react';
 import { View, Text, TextInput, Button, TouchableHighlight } from 'react-native';
 import {Actions} from 'react-native-router-flux';
 import { connect } from 'react-redux';
+import {modificaEmail, modificaSenha} from '../actions/AutenticacaoActions';
 
 const formLogin = props => {
-console.log(props);
     return(
     <View style={{ flex: 1, padding: 10 }}>
         <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
             <Text style={{ fontSize: 25 }}>Job Conversations</Text>
         </View>
         <View style={{ flex: 2 }}>
-            <TextInput value={props.email} style={{ fontSize: 20, height: 45 }} placeholder='Email' />
-            <TextInput value={props.senha} style={{ fontSize: 20, height: 45 }} placeholder='Senha' />
+            <TextInput value={props.email} style={{ fontSize: 20, height: 45 }} placeholder='Email' onChangeText={texto => props.modificaEmail(texto)} />
+            <TextInput value={props.senha} style={{ fontSize: 20, height: 45 }} placeholder='Senha' onChangeText={texto => props.modificaSenha(texto)} />
             <TouchableHighlight onPress={() => Actions.formCadastro()}>
             <Text style={{ fontSize: 20 }}>Ainda não tem cadastro? Cadastre aqui</Text>
             </TouchableHighlight>
@@ -33,4 +33,4 @@ const mapStateToProps = state => (
 
 )
 
-export default connect(mapStateToProps, null)(formLogin);
+export default connect(mapStateToProps, {modificaEmail, modificaSenha})(formLogin);
--- src/reducers/AutenticacaoReducer.js
+++ src/reducers/AutenticacaoReducer.js
@@ -5,7 +5,11 @@ const INITIAL_STATE = {
 }
 
 export default (state = INITIAL_STATE, action) => {
-
+    if(action.type == 'modifica_email'){
+        return {...state, email: action.payload}
+    }
+    if(action.type == 'modifica_senha'){
+        return {...state, senha: action.payload}
+    }
     return state;
-
 }
