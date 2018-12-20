694fa04672e9e4bc829c147cfcdd6bc1ce6a8634
Your Name <joaoloriato@outlook.com>
2018-12-20 13:37:36 -0200

    Mapping states to forward for Props

--- src/components/FormLogin.js
+++ src/components/FormLogin.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import { View, Text, TextInput, Button, TouchableHighlight } from 'react-native';
 import {Actions} from 'react-native-router-flux';
+import { connect } from 'react-redux';
 
-export default props => (
-
+const formLogin = props => {
+console.log(props);
+    return(
     <View style={{ flex: 1, padding: 10 }}>
         <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
             <Text style={{ fontSize: 25 }}>Job Conversations</Text>
         </View>
         <View style={{ flex: 2 }}>
-            <TextInput style={{ fontSize: 20, height: 45 }} placeholder='Email' />
-            <TextInput style={{ fontSize: 20, height: 45 }} placeholder='Senha' />
+            <TextInput value={props.email} style={{ fontSize: 20, height: 45 }} placeholder='Email' />
+            <TextInput value={props.senha} style={{ fontSize: 20, height: 45 }} placeholder='Senha' />
             <TouchableHighlight onPress={() => Actions.formCadastro()}>
             <Text style={{ fontSize: 20 }}>Ainda não tem cadastro? Cadastre aqui</Text>
             </TouchableHighlight>
@@ -20,3 +22,15 @@ export default props => (
         </View>
     </View>
 );
+}
+
+const mapStateToProps = state => (
+
+    {
+        email: state.AutenticacaoReducer.email,
+        senha: state.AutenticacaoReducer.senha
+    }
+
+)
+
+export default connect(mapStateToProps, null)(formLogin);
