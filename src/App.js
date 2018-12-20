db358d4426237db36197eaae86b8664877c0be10
Your Name <joaoloriato@outlook.com>
2018-12-20 12:50:58 -0200

    solving problems with react-native-router-flux version

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import React from 'react';
-import {Provider} from 'react-redux';
-import {createStore} from 'redux';
+import React from "react";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
 
-import Routes from './Routes';
+import Routes from "./Routes";
+import reducers from "./reducers/index"
 
 export default props => (
-    <Provider store={createStore(reducers)}>
+ <Provider store={createStore(reducers)}>
     <Routes />
-    </Provider>    
+  </Provider>
 );
