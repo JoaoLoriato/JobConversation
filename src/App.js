35db05db83abe6f0713772d398fd7089a0f7bc98
Your Name <joaoloriato@outlook.com>
2019-01-03 16:02:06 -0200

    Integrating Firebase

--- src/App.js
+++ src/App.js
@@ -1,12 +1,34 @@
-import React from "react";
+import React, {Component} from "react";
 import { Provider } from "react-redux";
 import { createStore } from "redux";
+import firebase from 'firebase';
 
 import Routes from "./Routes";
 import reducers from "./reducers/index"
 
-export default props => (
- <Provider store={createStore(reducers)}>
-    <Routes />
-  </Provider>
-);
+class App extends Component {
+
+  componentWillMount(){
+
+    firebase.initializeApp({
+      apiKey: "AIzaSyDeeegKm19ezxbkBMohoKTFXUtJY9y8IZg",
+      authDomain: "jobconversation.firebaseapp.com",
+      databaseURL: "https://jobconversation.firebaseio.com",
+      projectId: "jobconversation",
+      storageBucket: "jobconversation.appspot.com",
+      messagingSenderId: "609439388170"
+
+    });
+
+  }
+
+  render(){
+    return (
+     <Provider store={createStore(reducers)}>
+     <Routes />
+     </Provider> 
+    )
+  }
+};
+
+export default App;
