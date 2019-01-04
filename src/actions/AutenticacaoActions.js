93c61d8933c44d5cb57ade8146eb94001c17ce83
Your Name <joaoloriato@outlook.com>
2019-01-04 14:28:36 -0200

    Dispatch with Redux Thunk

--- src/actions/AutenticacaoActions.js
+++ src/actions/AutenticacaoActions.js
@@ -2,14 +2,14 @@ import firebase from 'firebase';
 
 export const modificaEmail = (texto) => {
     return {
-     type: 'modifica_email',
-     payload: texto
+        type: 'modifica_email',
+        payload: texto
 
     }
 }
 
 export const modificaSenha = (texto) => {
-    return{
+    return {
         type: 'modifica_senha',
         payload: texto
     }
@@ -23,13 +23,19 @@ export const modificaNome = (texto) => {
 }
 
 export const cadastraUsuario = ({nome, email, senha}) => {
-    
-    firebase.auth().createUserWithEmailAndPassword(email, senha)
-    .then(user => console.log(user))
-    .catch(erro => console.log(erro));
-    
-    return {
-        type: 'teste'
+
+    return dispatch => {
+        firebase.auth().createUserWithEmailAndPassword(email, senha)
+            .then(user => cadastroUsuarioSucesso(dispatch))
+            .catch(erro => cadastrousuarioErro(erro, dispatch));
     }
-    
+
+}
+
+const cadastroUsuarioSucesso = (dispatch) => {
+    dispatch( {type: 'sucesso'} );
+}
+
+const cadastrousuarioErro = (erro, dispatch) => {
+    dispatch( { type: 'erro' } );
 }
