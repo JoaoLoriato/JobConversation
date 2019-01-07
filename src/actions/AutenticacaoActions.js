5558df4ee61c28bf601bcda2bca2ae5137c94e0e
Your Name <joaoloriato@outlook.com>
2019-01-07 16:38:28 -0200

    Including B64 conversion for email signup

--- src/actions/AutenticacaoActions.js
+++ src/actions/AutenticacaoActions.js
@@ -1,5 +1,7 @@
 import firebase from 'firebase';
 import {Actions} from 'react-native-router-flux';
+import b64 from 'base-64';
+
 export const modificaEmail = (texto) => {
     return {
         type: 'modifica_email',
@@ -26,14 +28,19 @@ export const cadastraUsuario = ({nome, email, senha}) => {
 
     return dispatch => {
         firebase.auth().createUserWithEmailAndPassword(email, senha)
-            .then(user => cadastroUsuarioSucesso(dispatch))
+            .then(user => { 
+                let emailB64 =b64.encode(email);
+                firebase.database().ref('/contatos/'+emailB64)
+                .push({nome})
+                .then(value => cadastroUsuarioSucesso(dispatch))
+            })
             .catch(erro => cadastrousuarioErro(erro, dispatch));
     }
 
 }
 
 const cadastroUsuarioSucesso = (dispatch) => {
-    dispatch( {type: 'sucesso'} );
+    dispatch( {type: 'cadastro_usuario_sucesso'} );
     Actions.welcome();
 }
 
