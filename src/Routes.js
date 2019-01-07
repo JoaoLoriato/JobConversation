c0f71b891bdfaaa86183f6ae04207854d7e8cb5b
Your Name <joaoloriato@outlook.com>
2019-01-07 16:02:48 -0200

    Add Welcome View

--- src/Routes.js
+++ src/Routes.js
@@ -3,12 +3,13 @@ import {Router, Scene, Stack} from 'react-native-router-flux';
 
 import FormLogin from './components/FormLogin';
 import FormCadastro from './components/FormCadastro';
-
+import Welcome from './components/Welcome';
 export default props => (
     <Router>
         <Stack key="root">
         <Scene key='formLogin' component={FormLogin} title="Login"/>
         <Scene key='formCadastro' component={FormCadastro} title="Cadastro"/>
+        <Scene key='welcome' component={Welcome} title="Welcome" initial/>
         </Stack> 
     </Router>
 );
