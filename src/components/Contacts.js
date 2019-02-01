a8ced1eabe03b871c074193126102b4ffe353590
Joao Loriato <joaoloriato@outlook.com>
2019-02-01 11:37:42 -0200

    Introducing ListView in Contacts

--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -1,9 +1,25 @@
 import React, {Component} from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, ListView} from 'react-native';
 import {connect} from 'react-redux';
 import {contactUserFetch} from '../actions/AppActions';
+import _ from 'lodash';
 
 class Contacts extends Component {
+
+    constructor(props){
+        super(props)
+
+        const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2})
+
+        this.state = {dataOfSource: ds.cloneWithRows([
+            'Registro 1',
+            'Registro 2',
+            'Registro 3',
+            'Registro 4'
+
+        ])}
+    }
+
 
     componentWillMount(){
         this.props.contactUserFetch();
@@ -12,12 +28,23 @@ class Contacts extends Component {
 
     render(){
         return(
-            <View>
-                <Text>Contacts</Text>
-            </View>
+            <ListView
+                dataSource={this.state.dataOfSource}
+                renderRow={data => <View><Text>{data}</Text></View>}
+            />
         )
     }
     
+}
+
+mapStateToProps = state => {
+    const contacts = _.map(state.ListContactsReducer, (val, uid) => {
+        return {...val, uid}
+    })
+    console.log(contacts);
+    return {
+        
+    }
 }
 
 export default connect(null, {contactUserFetch})(Contacts);
--- src/reducers/ListContactsReducer.js
+++ src/reducers/ListContactsReducer.js
@@ -1,8 +1,11 @@
+import {LIST_CONTACT_USER} from '../actions/types';
+
 const INITIAL_STATE = {}
 
 export default (state = INITIAL_STATE, action) => {
-    console.log('teste', action.payload)
     switch (action.type) {
+        case LIST_CONTACT_USER:
+            return action.payload
         default:
             return state;
         
